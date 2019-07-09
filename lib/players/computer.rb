b932d78ab7706ea9db554adc0eb35624e90fbedf
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-09 19:05:15 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -2,7 +2,8 @@ module Players
   class Computer < Player
 
     def move(board)
-      if 
+      if board.cells[4] == " "
+        "5"
       elsif board.cells[0] == " "
         "1"
       elsif board.cells[8] == " "
@@ -15,10 +16,10 @@ module Players
         "3"
       elsif board.cells[6] == " "
         "7"
-      elsif board.cells[7] == " "
-        "8"
       elsif board.cells[5] == " "
         "6"
+      elsif board.cells[7] == " "
+        "8"
       end
     end
   end
