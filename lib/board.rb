8a0b507673c03370e6e86f017b0b478a6ffdadc6
Antonio Blandin <antonio.blandin25@gmail.com>
2019-07-09 19:06:41 +0000

    Automatically backed up by Learn

--- lib/board.rb
+++ lib/board.rb
@@ -13,9 +13,9 @@ class Board
   def display
     puts " #{self.cells[0]} | #{self.cells[1]} | #{self.cells[2]} "
     puts "-----------"
-    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
-    puts "-----------"
     puts " #{self.cells[6]} | #{self.cells[7]} | #{self.cells[8]} "
+    puts "-----------"
+    puts " #{self.cells[3]} | #{self.cells[4]} | #{self.cells[5]} "
   end
 
   def position(user_input)
